72859d7cdfd9419455a01c2ee2d7a193a544e3dd
201344033 <jsy2540@naver.com>
2016-09-30 12:32:12 +0900

    코딩

--- homework3/src/homework3/B01.java
+++ homework3/src/homework3/B01.java
@@ -4,8 +4,16 @@ public class B01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello");
-System.out.println("ㅎㅇ");
+		int n1,n2,n3;
+		int max,min;
+System.out.println("첫번째 숫자를 입력하세요  20");
+
+System.out.println("두번째 숫자를 입력하세요 80");
+
+System.out.println("세번째 숫자를 입력하세요 50");
+System.out.println("가장 큰 수는 80이고, 가장작은수는 20입니다");
+
+
 	}
 
 }
