b970a91b7744d1a36799be3946f90f968e2284d8
201344033 <jsy2540@naver.com>
2016-09-30 12:34:41 +0900

    코오딩

--- homework3/src/homework3/B01.java
+++ homework3/src/homework3/B01.java
@@ -6,12 +6,12 @@ public class B01 {
 		// TODO Auto-generated method stub
 		int n1,n2,n3;
 		int max,min;
-System.out.println("첫번째 숫자를 입력하세요  20");
+System.out.println("첫번째 숫자를 입력하세요  20\n");
 
-System.out.println("두번째 숫자를 입력하세요 80");
+System.out.println("두번째 숫자를 입력하세요 80\n");
 
-System.out.println("세번째 숫자를 입력하세요 50");
-System.out.println("가장 큰 수는 80이고, 가장작은수는 20입니다");
+System.out.println("세번째 숫자를 입력하세요 50\n");
+System.out.println("가장 큰 수는 80이고, 가장작은수는 20입니다\n");
 
 
 	}
